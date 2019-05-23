2eefe3c1cc54945344a59203236859221e9d14ff
xeno097 <sebyfaiella97@gmail.com>
2019-05-23 18:03:12 -0400

    added game of life solution in c++ with input text reading

---
+++ Week 1 Game of life/Game_of_life (C++) 2019-05-23/Game_of_life.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int rows,cols;
+char live_cell,dead_cell;
+char grid[1000][1000];
+
+int getcell(int x, int y){
+    if(x>=0 && x<rows && y>=0 && y<cols) return grid[x][y]=='*'? 1:0;
+    return 0;
+}
+
+void play(){
+
+    for(int x =0;x<rows;x++){
+        for(int y = 0;y<cols;y++){
+
+            int cells = 0;
+
+            cells+=getcell(x-1,y-1);
+            cells+=getcell(x,y-1);
+            cells+=getcell(x+1,y-1);
+            cells+=getcell(x-1,y);
+            cells+=getcell(x+1,y);
+            cells+=getcell(x-1,y+1);
+            cells+=getcell(x,y+1);
+            cells+=getcell(x+1,y+1);
+
+            if(cells<2||cells>3) grid[x][y]=dead_cell;
+            else if(cells==3) grid[x][y]=live_cell;
+        }
+    }
+}
+
+void print_grid(){
+    for(int x = 0;x<rows; x++){
+        for(int y =0;y<cols;y++){
+            cout << grid[x][y];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main() {
+
+    ifstream file("S:\\Desktop\\test.txt"); // replace with the directory of your test file
+    string str;
+
+    getline(file,str);
+    rows = str[0]-'0';
+    cols = str[2]-'0';
+    live_cell = str[4];
+    dead_cell = str[6];
+
+    int x = 0;
+    while(getline(file,str)){
+        for(int y = 0;y<cols;y++){
+            grid[x][y]=str[y];
+        }
+        x++;
+    }
+
+    print_grid();
+    play();
+    print_grid();
+    return 0;
+}
