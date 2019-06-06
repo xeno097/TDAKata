ac2fcd12aab473f6fc8078822ade24428c81f9c0
xeno097 <sebyfaiella97@gmail.com>
2019-06-06 11:29:09 -0400

    Range Kata solution in c# added not so sure the Overlaps method is right

---
+++ Week 3 Range/RangeCalculatorKata (TDD) 2019-06-06/RangeCalculatorKata/RangeCalculator.cs
@@ -0,0 +1,98 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace RangeCalculatorKata
+{
+    public class RangeCalculator
+    {
+        int low;
+        int high;
+
+        public RangeCalculator(string range)
+        {
+            string[] bounds = range.Substring(1, range.Length - 2).Split(",");
+            this.low = GetLowerBound(range[0], bounds[0]);
+            this.high = GetUpperBound(range[range.Length - 1], bounds[1]);
+        }
+
+        private int GetLowerBound(char parentesis, string num)
+        {
+            int ret = Convert.ToInt32(num);
+            return parentesis == '[' ? ret : ret + 1;
+        }
+
+        private int GetUpperBound(char parentesis, string num)
+        {
+            int ret = Convert.ToInt32(num);
+            return parentesis == ']' ? ret : ret - 1; 
+
+        }
+
+        public bool Range(string tocheck)
+        {
+            string[] numsToCheck = tocheck.Substring(1, tocheck.Length - 2).Split(",");
+            
+            foreach(string n in numsToCheck)
+            {
+                int v = Convert.ToInt32(n);
+                if(!(v>=low && v<=high))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool Contains(string toCheck)
+        {
+            string[] bounds = toCheck.Substring(1, toCheck.Length - 2).Split(",");
+            int toCheckLow = GetLowerBound(toCheck[0], bounds[0]);
+            int toCheckHigh = GetUpperBound(toCheck[toCheck.Length - 1], bounds[1]);
+
+            return (toCheckLow >= this.low && toCheckHigh <= this.high);
+        }
+
+        public string GetAllPoints()
+        {
+            string ret = "{";
+           
+            for(int i = this.low; i <= this.high;i++)
+                ret += Convert.ToString(i) + ',';
+
+            ret = ret.Substring(0,ret.Length-1) + '}';
+
+            return ret;
+        }
+
+        public string EndPoints()
+        {
+            string ret = "{" + Convert.ToString(this.low) + "," + Convert.ToString(this.high) + "}";
+            return ret;
+        }
+
+        public bool AreEquals(string toCheck)
+        {
+            string[] bounds = toCheck.Substring(1, toCheck.Length - 2).Split(",");
+            int toCheckLow = GetLowerBound(toCheck[0], bounds[0]);
+            int toCheckHigh = GetUpperBound(toCheck[toCheck.Length - 1], bounds[1]);
+
+            return (toCheckLow == this.low && toCheckHigh == this.high);
+        }
+
+        public bool OverLaps(string toCheck)
+        {
+            bool ret = false;
+            string[] bounds = toCheck.Substring(1, toCheck.Length - 2).Split(",");
+            int toCheckLow = GetLowerBound(toCheck[0], bounds[0]);
+            int toCheckHigh = GetUpperBound(toCheck[toCheck.Length - 1], bounds[1]);
+
+            if (this.low <= toCheckLow && this.high >= toCheckHigh) return true;
+            else if (this.low >= toCheckLow && this.high <= toCheckHigh) return true;
+
+            return ret;
+        }
+    }
+}
---
+++ Week 3 Range/RangeCalculatorKata (TDD) 2019-06-06/RangeCalculatorTests/RangeTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using RangeCalculatorKata;
+
+namespace RangeCalculatorTests
+{
+    [TestFixture]
+    class RangeTests
+    {
+        [Test]
+        public void WithLow2andHigh6_contains4_ReturnTrue()
+        {
+            RangeCalculator rangeCalculator = new RangeCalculator("[2,6)");
+            Assert.AreEqual(rangeCalculator.Range("{4}"),true);
+        }
+
+        [Test]
+        public void WithLow2andHigh6_containsList_ReturnFalse()
+        {
+            RangeCalculator rangeCalculator = new RangeCalculator("[2,6)");
+            Assert.AreEqual(rangeCalculator.Range("{-1,1,6,10}"), false);
+        }
+
+        [Test]
+        public void WithLow2andHigh6_getAllPints_Returns2345()
+        {
+            RangeCalculator rangeCalculator = new RangeCalculator("[2,6)");
+            Assert.AreEqual(rangeCalculator.GetAllPoints(), "{2,3,4,5}");
+        }
+
+        [Test]
+        public void WithLow2andHigh5_contains_Range710()
+        {
+            RangeCalculator rangeCalculator = new RangeCalculator("[2,6)");
+            Assert.AreEqual(rangeCalculator.Contains("[7,10)"), false);
+        }
+
+        [Test]
+        public void WithLow2andHigh5_returns25()
+        {
+            RangeCalculator rangeCalculator = new RangeCalculator("[2,6)");
+            Assert.AreEqual(rangeCalculator.EndPoints(), "{2,5}");
+        }
+
+        [Test]
+        public void WithLow2andHigh4_doesNotOverlaps_low7high10()
+        {
+            RangeCalculator rangeCalculator = new RangeCalculator("[2,5)");
+            Assert.AreEqual(rangeCalculator.OverLaps("[7,10)"), false);
+        }
+
+        [Test]
+        public void WithLow3andHigh5_areequals_low3high5()
+        {
+            RangeCalculator rangeCalculator = new RangeCalculator("[3,5)");
+            Assert.AreEqual(rangeCalculator.AreEquals("[3,5)"), true);
+        }
+
+    }
+}
