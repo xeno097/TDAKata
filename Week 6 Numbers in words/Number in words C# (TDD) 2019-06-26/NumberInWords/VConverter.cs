7cdcf63588afb122553f30ba2f7ee022b7ef1f56
xeno097 <sebyfaiella97@gmail.com>
2019-06-26 15:58:02 -0400

    numbers in word c# solution till 9999(number to string only) added

---
+++ Week 6 Numbers in words/Number in words C# (TDD) 2019-06-26/NumberInWords/VConverter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberInWords
+{
+
+    public class VConverter
+    {
+
+        Dictionary<int, string> numstrings;
+
+        public VConverter()
+        {
+            this.numstrings = new Dictionary<int, string>();
+
+            this.numstrings.Add(1, "one");
+            this.numstrings.Add(2, "two");
+            this.numstrings.Add(3, "three");
+            this.numstrings.Add(4, "four");
+            this.numstrings.Add(5, "five");
+            this.numstrings.Add(6, "six");
+            this.numstrings.Add(7, "seven");
+            this.numstrings.Add(8, "eight");
+            this.numstrings.Add(9, "nine");
+            this.numstrings.Add(10, "ten");
+            this.numstrings.Add(11, "eleven");
+            this.numstrings.Add(12, "twelve");
+            this.numstrings.Add(13, "thirteen");
+            this.numstrings.Add(14, "fourteen");
+            this.numstrings.Add(15, "fifteen");
+            this.numstrings.Add(16, "sixteen");
+            this.numstrings.Add(17, "seventeen");
+            this.numstrings.Add(18, "eighteen");
+            this.numstrings.Add(19, "nineteen");
+            this.numstrings.Add(20, "twenty");
+            this.numstrings.Add(30, "thirty");
+            this.numstrings.Add(40, "forty");
+            this.numstrings.Add(50, "fifty");
+            this.numstrings.Add(60, "sixty");
+            this.numstrings.Add(70, "seventy");
+            this.numstrings.Add(80, "eighty");
+            this.numstrings.Add(90, "ninety");
+            this.numstrings.Add(100, "hundred");
+            this.numstrings.Add(1000, "thousand");
+        }
+
+
+        int Pow10(int x)
+        {
+            int ret = 1;
+            for(int i = 0; i < x; i++)
+            {
+                ret *= 10;
+            }
+            return ret;
+        }
+
+        public string NumToString(int num)
+        {
+            if (num == 1)
+            {
+                return "one dollar";
+            }
+            string ret = "";
+
+            string to_string = Convert.ToString(num);
+            int len = to_string.Length - 1;
+
+            if (num < 21)
+            {
+                ret = numstrings[num] + ' ';
+            }
+            else
+            {
+                foreach (char c in to_string)
+                {
+
+                    if (len < 2)
+                    {
+                        int digit = c - '0';
+                        int key = digit * this.Pow10(len);
+                        ret += numstrings[key] + ' ';
+                    }
+                    else
+                    {
+                        int digit = c - '0';
+                        int key = this.Pow10(len);
+                        ret += numstrings[digit] +' '+ numstrings[key] + ' ';
+                    }
+                    len--;
+                }
+            }
+
+
+            ret += "dollars";
+
+            return ret;
+            
+        } 
+    }
+}
---
+++ Week 6 Numbers in words/Number in words C# (TDD) 2019-06-26/NumberInwordsTests/NumInwordsTests.cs
@@ -0,0 +1,55 @@
+﻿using NumberInWords;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberInwordsTests
+{
+    [TestFixture]
+    public class NumInwordsTests
+    {
+        [Test]
+        public void With1_returns_onedollar()
+        {
+            VConverter vConverter = new VConverter();
+            Assert.AreEqual("one dollar", vConverter.NumToString(1));
+        }
+
+        [Test]
+        public void With15_returns_fifteendollars()
+        {
+            VConverter vConverter = new VConverter();
+            Assert.AreEqual("fifteen dollars", vConverter.NumToString(15));
+        }
+
+        [Test]
+        public void With23_returns_twentythreedollars()
+        {
+            VConverter vConverter = new VConverter();
+            Assert.AreEqual("twenty three dollars", vConverter.NumToString(23));
+        }
+
+        [Test]
+        public void With57_returns_fiftysevendollars()
+        {
+            VConverter vConverter = new VConverter();
+            Assert.AreEqual("fifty seven dollars", vConverter.NumToString(57));
+        }
+
+        [Test]
+        public void With257_returns_twohundredfiftysevendollars()
+        {
+            VConverter vConverter = new VConverter();
+            Assert.AreEqual("two hundred fifty seven dollars", vConverter.NumToString(257));
+        }
+
+        [Test]
+        public void With9999_returns_ninethousandninehundredninetyninedollars()
+        {
+            VConverter vConverter = new VConverter();
+            Assert.AreEqual("nine thousand nine hundred ninety nine dollars", vConverter.NumToString(9999));
+        }
+
+    }
+}
